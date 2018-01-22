51aa401cef44f04ef8668ee8d081d44010cf4790
Bill Levay <wjlevay@gmail.com>
2018-01-21 18:18:04 -0800

    allow user to input year

--- uploadBAR.py
+++ uploadBAR.py
@@ -7,7 +7,7 @@
 # by Bill Levay
 ###
 
-import zipfile, os, datetime, gspread, subprocess
+import zipfile, os, datetime, gspread, subprocess, sys
 from internetarchive import upload
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -99,12 +99,16 @@ def process():
 
 	process_list = []
 
-	for issue in os.listdir(source_path):
-		if len(issue) == 8:
-			process_list.append(issue)
-	print process_list
-	return process_list
+	if os.path.exists(source_path):
 
+		for issue in os.listdir(source_path):
+			if len(issue) == 8:
+				process_list.append(issue)
+		print process_list
+		return process_list
+
+	else:
+		print 'Sorry, we cannot find any issues from', year
 
 
 ###
@@ -163,36 +167,76 @@ def upload(issue):
 		pass
 
 
+###
+# Yes or No?
+###
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
 
 ###
 # Start processing
 ###
 
 if __name__ == "__main__":
-	years = []
 
-	for year in years:
+	year = raw_input('Enter the year you want to upload to Internet Archive: ')
 
-		source_path = 'G:\\Dropbox (GLBTHS)\\Archive\\BAR\\{}\\'.format(year)
+	source_path = 'G:\\Dropbox (GLBTHS)\\Archive\\BAR\\{}\\'.format(year)
 
-		process_list = process()
+	process_list = process()
 
-		for issue in process_list:
-			issue_path = source_path + issue
-			issue_meta = get_metadata(issue)
+	if process_list is not None:
 
-			# check to make sure we didn't already upload this one
-			if issue_meta['ia_upload'] == '':
+		if query_yes_no('Upload ' + str(len(process_list)) + ' issues from ' + year + '?', None):
 
-				zip_path = 'G:\\{}_images.zip'.format(issue_meta['ia_id'])
-				zip(issue)
-				upload(issue)
-				update_sheet(issue)
-				print 'Finished with', issue, '- moving on to next issue.'
+			for issue in process_list:
+				issue_path = source_path + issue
+				issue_meta = get_metadata(issue)
 
-			else:
-				print issue, 'was already uploaded to IA. Moving to next issue.'
+				# check to make sure we didn't already upload this one
+				if issue_meta['ia_upload'] == '':
 
-		print 'Finished with', year, '- moving on...'
+					zip_path = 'G:\\{}_images.zip'.format(issue_meta['ia_id'])
+					zip(issue)
+					upload(issue)
+					update_sheet(issue)
+					print 'Finished with', issue, '- moving on to next issue.'
+
+				else:
+					print issue, 'was already uploaded to IA. Moving to next issue.'
+		
+		else:
+			print('No issues uploaded.')
+	else:
+		print('No issues to upload.')
 		
 	print 'ALL DONE'
